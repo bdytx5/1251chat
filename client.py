2a46c8e93d080dfe54f8e02bab1ce715068c11db
bdytx5 <bdytx5@mail.missouri.edu>
2019-11-21 13:26:56 -0600

    py3 support

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 my_socket = socket.socket()
 my_socket.connect(('127.0.0.1', 8820))
-message = raw_input('Enter some data: ')
-my_socket.send(message)
+message = input('Enter some data: ')
+my_socket.send(message.encode("utf-8"))
 response_data = my_socket.recv(1024)
 print("Received: %s" % response_data)
 my_socket.close
