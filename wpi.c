1239cf06d8862c93fb422ec3cb649fccf6945a41
bdytx5 <bdytx5@mail.missouri.edu>
2019-11-24 12:23:16 -0600

    fixed wpi

--- wpi.c
+++ wpi.c
@@ -7,19 +7,28 @@
 int main()
 {
     wiringPiSetupGpio () ;
-	pinMode (4, OUTPUT) ; //green
-	digitalWrite (2,1); // initially on 
-	pinMode (6, OUTPUT) ;//SPKR
+	pinMode (2, OUTPUT) ; //green
+        pinMode (3, OUTPUT) ; //green
+        pinMode (4, OUTPUT) ; //green
+        pinMode (5, OUTPUT) ; //green
+        digitalWrite (2,0); // initially on
+        digitalWrite (3,0); // initially on
+        digitalWrite (4,1); // initially on
 
+
+	digitalWrite (5,0); // initially on 
+	pinMode (6, OUTPUT) ;//SPKR
+    int freq = 300;
     int i = 0;
-    while(i<50){
+    while(i<1000){
         digitalWrite (6,1); // turn on spkr
         i++;
-        usleep(300);
+        usleep(freq);
         digitalWrite (6,0); // shut off spkr
-    }
+	usleep(freq);    
+}
     digitalWrite (6,0); // shut off 
-	digitalWrite (2,0); // shut off 
+	digitalWrite (4,0); // shut off 
 
    printf("done");
    return 0;
